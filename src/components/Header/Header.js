44fa510614b7ed0200c08438a7d9a6d33fcdb232
Sakib12345 <sakibalislam12345@gmail.com>
2021-03-02 11:08:56 +0600

    header done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Header.css';
+import MenuSharpIcon from '@material-ui/icons/MenuSharp';
+import SearchSharpIcon from '@material-ui/icons/SearchSharp';
+import VideoCallSharpIcon from '@material-ui/icons/VideoCallSharp';
+import AppsSharpIcon from '@material-ui/icons/AppsSharp';
+import NotificationsSharpIcon from '@material-ui/icons/NotificationsSharp';
+import Avatar from '@material-ui/core/Avatar';
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header left">
+                <MenuSharpIcon></MenuSharpIcon>
+                <img
+                className="header-logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt=""/>
+            </div>
+            
+            <div className="header-input">
+                <input 
+                type="text"
+                placeholder="Search"
+                />
+                <SearchSharpIcon className="header-inputButton"></SearchSharpIcon>
+            </div>
+            
+            <div className="header-icons">
+                <VideoCallSharpIcon className='VideoCallSharpIcon'></VideoCallSharpIcon>
+                <AppsSharpIcon  className='AppsSharpIcon'></AppsSharpIcon>
+                <NotificationsSharpIcon  className='NotificationsSharpIcon'></NotificationsSharpIcon>
+                <Avatar
+                alt='Sakib'
+                src='https://www.doesport.co.uk/wp-content/uploads/2017/11/profile-icon-male-avatar-portrait-casual-person-silhouette-face-flat-design-vector-illustration-58249394.jpg'
+                ></Avatar>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDOze3LkOw9g6JA00bHsvlSA35J6aAW1dw",
+    authDomain: "clone-50e2a.firebaseapp.com",
+    projectId: "clone-50e2a",
+    storageBucket: "clone-50e2a.appspot.com",
+    messagingSenderId: "636863960974",
+    appId: "1:636863960974:web:c47bc9fb5b7312f2a3b6df",
+    measurementId: "G-L5720TM65B"
+  };
